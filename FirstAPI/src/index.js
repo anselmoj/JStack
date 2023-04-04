ff63ecaf8e498c683e97d9db8a40df8376f6226b
anselmoj <juniorpr.comp@gmail.com>
2023-04-04 08:53:10 -0300

    feat: adicionando babel

--- FirstAPI/src/index.js
+++ FirstAPI/src/index.js
@@ -1,15 +1,37 @@
 const http = require('http')
-const UserController = require('./controllers/UserController')
+const routes = require('./routes')
+const { URL } = require('url')
 
 const server = http.createServer((request, response) => {
+  const parsedUrl = new URL(`http://localhost:3000${request.url}`)
+  console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`)
 
-  console.log(`Request method: ${request.method} | Endpoint: ${request.url}`)
+  let { pathname } = parsedUrl
+  let id = null
 
-  if(request.url === '/users' && request.method === 'GET') {
-    UserController.listUsers(request, response)
+  const splitEndPoint = pathname.split('/').filter(Boolean)
+  if(splitEndPoint.length > 1) {
+    pathname = `/${splitEndPoint[0]}/:id`
+    id = splitEndPoint[1]
+  }
+
+  const route = routes.find((routeObj) => (
+    routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+  ))
+ 
+  if(route) {
+    request.query = Object.fromEntries(parsedUrl.searchParams)
+    request.params = { id }
+
+    response.send = (statusCode, body) => {
+      response.writeHead(statusCode, {'Content-Type': 'application/json'})
+      response.end(JSON.stringify(body))
+    }
+
+    route.handler(request, response)
   } else {
     response.writeHead(404, {'Content-Type': 'text/html'})
-    response.end(`Cannot ${request.method} ${request.url}`)
+    response.end(`Cannot ${request.method} ${parsedUrl.pathname}`)
   }
 })
 
