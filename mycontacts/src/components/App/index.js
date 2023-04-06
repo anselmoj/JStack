6c3ea9b9be53fb4b31d70b7f4ddac76e1fc59690
anselmoj <juniorpr.comp@gmail.com>
2023-04-06 17:10:55 -0300

    feat: adicionando estilos

--- mycontacts/src/components/App/index.js
+++ mycontacts/src/components/App/index.js
@@ -3,6 +3,7 @@ import GlobalStyle from '../../assets/styles/global';
 import defaultTheme from '../../assets/styles/themes/default';
 import { Container } from './styles';
 import Header from '../Header';
+import ContactList from '../ContactList';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <GlobalStyle />
       <Container>
         <Header />
+        <ContactList />
       </Container>
     </ThemeProvider>
   );
